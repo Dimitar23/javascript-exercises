a25c4019e34f3957bd862a95f08bbb5e3cf7daec
Dimitar Cvetanoski <dimitar.cv23@gmail.com>
2023-05-07 21:21:24 +0200

    Completed Fundamentals 4 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    let sum = 0;
 
+    if(n > m){
+        let temp = n;
+        n = m;
+        m = temp;
+    }
+
+    if(typeof(n) != 'number' || typeof(m) != 'number' || n < 0 || m < 0) return "ERROR";
+
+    for(let i = n; i <= m; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
